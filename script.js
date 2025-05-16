12f24ff5e4171bcf83ddc5855d22e4b7a66d6006
Marcin <cinek13378@wp.pl>
2025-05-16 19:45:41 +0200

    Init

---
+++ script.js
@@ -0,0 +1,39 @@
+const submitBtn = document.getElementById("submitBtn");
+const pokemonFrontImg = document.getElementById("pokemonFrontImg");
+const pokemonBackImg = document.getElementById("pokemonBackImg");
+const pokemonTypes = document.getElementById("pokemonTypes");
+
+submitBtn.addEventListener('click', () =>{
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    getPokemonData(pokemonName);
+})
+
+
+
+async function getPokemonData(pokemonName){
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    if(!response.ok){
+        throw new Error("Fetch went wrong!");
+    }
+    const data = await response.json();
+    console.log(data);
+
+    const pokemonSpriteFront = data.sprites.front_default;
+    const pokemonSpriteBack = data.sprites.back_default;
+    pokemonFrontImg.src = pokemonSpriteFront;
+    pokemonFrontImg.style.display = "inline";
+    pokemonBackImg.src = pokemonSpriteBack;
+    pokemonBackImg.style.display = "inline";
+
+
+
+    let pokemonTypesContent = "";
+    data.types.forEach(type => {
+        pokemonTypesContent += type.type.name + " ";
+    });
+
+    pokemonTypes.textContent = pokemonTypesContent;
+    pokemonTypes.style.display = "block";
+
+}
